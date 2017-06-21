40738af473145ec9f0715aea642cc8ef4e12ab4c
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-21 14:32:29 +0200

    variable declaration

---
+++ src/bank_account_manager/Variables.java
@@ -0,0 +1,11 @@
+package bank_account_manager;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// variable declaration
+		char type_compte = '\0', moyen_paiement = '\0', theme = '\0';
+		double valeur_courante = 0.0;
+		long num_compte = 0;
+	}
+}
