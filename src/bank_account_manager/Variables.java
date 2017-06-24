4d8e3265e0fd7009e67d952b9dab3e64624f809f
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-24 18:57:44 +0200

    adding some switch

--- src/bank_account_manager/Variables.java
+++ src/bank_account_manager/Variables.java
@@ -25,50 +25,61 @@ public class Variables {
 		// user's choice
 		choice = userInput.nextByte();
 
-		// if choice 1 : create account
-		System.out.println("Choisir le type de compte (C)ourant, (J)oint, (E)pargne ");
-		account_type = userInput.next().charAt(0);
-		System.out.println("Numero du compte");
-		account_num = userInput.nextLong();
-		System.out.println("Premiere valeur creditee");
-		current_value = userInput.nextDouble();
-		System.out.println("Taux de placement");
-		rate = userInput.nextDouble();
-
+		switch (choice) {
+		case 1 :
+			// if choice 1 : create account
+			System.out.println("Choisir le type de compte (C)ourant, (J)oint, (E)pargne ");
+			account_type = userInput.next().charAt(0);
+			System.out.println("Numero du compte");
+			account_num = userInput.nextLong();
+			System.out.println("Premiere valeur creditee");
+			current_value = userInput.nextDouble();
+			System.out.println("Taux de placement");
+			rate = userInput.nextDouble();
+			break;
+		
 		// if choice 2 : print account
-		System.out.println("Quel compte souhaitez vous consulter ?");
-		account_num = userInput.nextLong();
-		// if account does not exist
-		System.out.println("Le compte numéro : " + account_num + " n'est pas reconnu par le système.");
-		// if account exist and its an epargne print current_value and its rate
-		System.out.println("Le taux du compte numéro : " + account_num + " est à : " + rate);
-		System.out.println("La valeur courante du compte numéro : " + account_num + " est de : " + current_value);
-		// if account exist and its not an epargne print current_value
-		System.out.println("La valeur courante du compte numero : " + account_num + " est de : " + current_value);
-
+		case 2 :
+			System.out.println("Quel compte souhaitez vous consulter ?");
+			account_num = userInput.nextLong();
+			// if account does not exist
+			System.out.println("Le compte numéro : " + account_num + " n'est pas reconnu par le système.");
+			// if account exist and its an epargne print current_value and its rate
+			System.out.println("Le taux du compte numéro : " + account_num + " est à : " + rate);
+			System.out.println("La valeur courante du compte numéro : " + account_num + " est de : " + current_value);
+			// if account exist and its not an epargne print current_value
+			System.out.println("La valeur courante du compte numero : " + account_num + " est de : " + current_value);
+			break;
+		
 		// if choice 3 : new operation creation
-		System.out.println("Option non programmee");
-
+		case 3 :
+			System.out.println("Option non programmee");
+			break;
+		
 		// if choice 4 : exit
-		System.out.print("Au revoir");
-		System.exit(0);
-
+		case 4 :
+			System.out.print("Au revoir");
+			System.exit(0);
+			break;
+		
 		// if choice 5
-		System.out.println("Option 1 :");
-		System.out.println("Pour créer un compte courante entrer : C"); 
-		System.out.println("Pour créer un compte joint entrer : J");
-		System.out.println("Pour créer un compte epargne entrer : E");
-		System.out.println("	- Entrer ensuite le numero du compte");
-		System.out.println("	- Entrer alors la valeur creditee");
-		System.out.println("Dans le cas d'un compte epargne, entrer le taux.");
-		System.out.println("Option 2 :");
-		System.out.println("Le systeme affiche les donnees du compte choisi");
-		System.out.println("Option 3 :");
-		System.out.println("Ecrire une ligne comptable");
-		System.out.println("Option 4 :");
-		System.out.println("Quitter le programme");
-		System.out.println("Option 5 :");
-		System.out.println("Afficher de l'aide : Vous y etes ;) ");
-
+		case 5 :
+			System.out.println("Option 1 :");
+			System.out.println("Pour créer un compte courante entrer : C"); 
+			System.out.println("Pour créer un compte joint entrer : J");
+			System.out.println("Pour créer un compte epargne entrer : E");
+			System.out.println("	- Entrer ensuite le numero du compte");
+			System.out.println("	- Entrer alors la valeur creditee");
+			System.out.println("Dans le cas d'un compte epargne, entrer le taux.");
+			System.out.println("Option 2 :");
+			System.out.println("Le systeme affiche les donnees du compte choisi");
+			System.out.println("Option 3 :");
+			System.out.println("Ecrire une ligne comptable");
+			System.out.println("Option 4 :");
+			System.out.println("Quitter le programme");
+			System.out.println("Option 5 :");
+			System.out.println("Afficher de l'aide : Vous y etes ;) ");
+			break;
+		}
 	}
 }
