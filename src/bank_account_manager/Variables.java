b1680af2a993069316b6ab1a072196f728ac6a04
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-26 17:00:09 +0200

    adding first do..while for the whole app

--- src/bank_account_manager/Variables.java
+++ src/bank_account_manager/Variables.java
@@ -14,84 +14,85 @@ public class Variables {
 		long account_num_entered = 0;
 
 		Scanner userInput = new Scanner(System.in);
-
-		// printing menu
-		System.out.println("1. Creer un compte");	
-		System.out.println("2. Afficher un compte");
-		System.out.println("3. Creer une ligne comptable");
-		System.out.println("4. Sortir");
-		System.out.println("5. De l'aide");
-		System.out.print("Votre choix : ");
 		
-		// user's choice
-		choice = userInput.nextByte();
-
-		switch (choice) {
-		case 1 :
-			// if choice 1 : create account
-			System.out.println("Choisir le type de compte (C)ourant, (J)oint, (E)pargne :");
-			account_type = userInput.next().charAt(0);
-			System.out.println("Numero du compte :");
-			account_num_created = userInput.nextLong();
-			System.out.println("Premiere valeur creditee :");
-			current_value = userInput.nextDouble();
+		do {
+			// printing menu
+			System.out.println("1. Creer un compte");	
+			System.out.println("2. Afficher un compte");
+			System.out.println("3. Creer une ligne comptable");
+			System.out.println("4. Sortir");
+			System.out.println("5. De l'aide");
+			System.out.print("Votre choix : ");
 			
-			// if (E)pargne = ask for rate 
-			if (account_type == 'E') {
-				System.out.println("Choisir le taux de placement :");
-				rate = userInput.nextDouble();
-			}
-			break;
-		
-		// if choice 2 : print account
-		case 2 :
-			System.out.println("Quel compte souhaitez vous consulter ?");
-			account_num_entered = userInput.nextLong();
-			// if account exist and its an epargne print current_value and its rate
-				// account exist
-			if (account_num_entered == account_num_created) {
-				System.out.println("Le compte numéro : " + account_num_created + "est un compte : ");
-				if (account_type == 'C') System.out.println(" courant");
-				else if (account_type == 'J') System.out.println(" joint");
-				else if (account_type == 'E') {
-					System.out.println(" epargne dont le taux est de : " + rate);
-				}	
-				System.out.println("La valeur courante du compte est de : " + current_value);
-				// account does not exist 
-			} else System.out.println("Le compte numéro : " + account_num_entered + " n'est pas reconnu par le système.");	
-			break;
-		
-		// if choice 3 : new operation creation
-		case 3 :
-			System.out.println("Option non programmee");
-			break;
-		
-		// if choice 4 : exit
-		case 4 :
-			System.out.print("Au revoir");
-			System.exit(0);
-			break;
-		
-		// if choice 5
-		case 5 :
-			System.out.println("Option 1 :");
-			System.out.println("Pour créer un compte courante entrer : C"); 
-			System.out.println("Pour créer un compte joint entrer : J");
-			System.out.println("Pour créer un compte epargne entrer : E");
-			System.out.println("	- Entrer ensuite le numero du compte");
-			System.out.println("	- Entrer alors la valeur creditee");
-			System.out.println("Dans le cas d'un compte epargne, entrer le taux.");
-			System.out.println("Option 2 :");
-			System.out.println("Le systeme affiche les donnees du compte choisi");
-			System.out.println("Option 3 :");
-			System.out.println("Ecrire une ligne comptable");
-			System.out.println("Option 4 :");
-			System.out.println("Quitter le programme");
-			System.out.println("Option 5 :");
-			System.out.println("Afficher de l'aide : Vous y etes ;) ");
-			break;
-		}
-		
-		
+			// user's choice
+			choice = userInput.nextByte();
+
+			switch (choice) {
+			case 1 :
+				// if choice 1 : create account
+				System.out.println("Choisir le type de compte (C)ourant, (J)oint, (E)pargne :");
+				account_type = userInput.next().charAt(0);
+				System.out.println("Numero du compte :");
+				account_num_created = userInput.nextLong();
+				System.out.println("Premiere valeur creditee :");
+				current_value = userInput.nextDouble();
+				
+				// if (E)pargne = ask for rate 
+				if (account_type == 'E') {
+					System.out.println("Choisir le taux de placement :");
+					rate = userInput.nextDouble();
+				}
+				break;
+			
+			// if choice 2 : print account
+			case 2 :
+				System.out.println("Quel compte souhaitez vous consulter ?");
+				account_num_entered = userInput.nextLong();
+				// if account exist and its an epargne print current_value and its rate
+					// account exist
+				if (account_num_entered == account_num_created) {
+					System.out.println("Le compte numéro : " + account_num_created + " est un compte : ");
+					if (account_type == 'C') System.out.print(" courant");
+					else if (account_type == 'J') System.out.print(" joint");
+					else if (account_type == 'E') {
+						System.out.println(" epargne");
+						System.out.println(" dont le taux est de : " + rate);
+					}	
+					System.out.println("La valeur courante du compte est de : " + current_value);
+					// account does not exist 
+				} else System.out.println("Le compte numéro : " + account_num_entered + " n'est pas reconnu par le système.");	
+				break;
+			
+			// if choice 3 : new operation creation
+			case 3 :
+				System.out.println("Option non programmee");
+				break;
+			
+			// if choice 4 : exit
+			case 4 :
+				System.out.print("Au revoir");
+				System.exit(0);
+				break;
+			
+			// if choice 5
+			case 5 :
+				System.out.println("Option 1 :");
+				System.out.println("Pour créer un compte courante entrer : C"); 
+				System.out.println("Pour créer un compte joint entrer : J");
+				System.out.println("Pour créer un compte epargne entrer : E");
+				System.out.println("	- Entrer ensuite le numero du compte");
+				System.out.println("	- Entrer alors la valeur creditee");
+				System.out.println("Dans le cas d'un compte epargne, entrer le taux.");
+				System.out.println("Option 2 :");
+				System.out.println("Le systeme affiche les donnees du compte choisi");
+				System.out.println("Option 3 :");
+				System.out.println("Ecrire une ligne comptable");
+				System.out.println("Option 4 :");
+				System.out.println("Quitter le programme");
+				System.out.println("Option 5 :");
+				System.out.println("Afficher de l'aide : Vous y etes ;) ");
+				break;
+			}		
+		} while (choice != 4);	
 	}
 }
