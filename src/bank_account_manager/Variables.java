cd50ab91d3f9b6ec4fe6ab82ca4d51339a1735cf
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-27 11:36:15 +0200

    refactoring with some OOP

--- src/bank_account_manager/Variables.java
+++ src/bank_account_manager/Variables.java
@@ -12,25 +12,13 @@ public class Variables {
 		double rate = 0.0;
 		long account_num_created = 0;
 		long account_num_entered = 0;
-
 		Scanner userInput = new Scanner(System.in);
 		
 		do {
-			// printing menu
-			System.out.println("1. Creer un compte");	
-			System.out.println("2. Afficher un compte");
-			System.out.println("3. Creer une ligne comptable");
-			System.out.println("4. Sortir");
-			System.out.println("5. De l'aide");
-			System.out.print("Votre choix : ");
-			
-			// user's choice
-			choice = userInput.nextByte();
-
+			choice = mainMenu();
 			switch (choice) {
 			case 1 :
 				do {
-					// if choice 1 : create account
 					System.out.print("Choisir le type de compte (C)ourant, (J)oint, (E)pargne : ");
 					account_type = userInput.next().charAt(0);
 				} while (account_type != 'C' && account_type != 'J' && account_type != 'E' );
@@ -72,32 +60,58 @@ public class Variables {
 			
 			// if choice 4 : exit
 			case 4 :
-				System.out.print("Au revoir");
-				System.exit(0);
+				out();
 				break;
 			
 			// if choice 5
 			case 5 :
-				System.out.println("Option 1 :");
-				System.out.println("Pour créer un compte courante entrer : C"); 
-				System.out.println("Pour créer un compte joint entrer : J");
-				System.out.println("Pour créer un compte epargne entrer : E");
-				System.out.println("	- Entrer ensuite le numero du compte");
-				System.out.println("	- Entrer alors la valeur creditee");
-				System.out.println("Dans le cas d'un compte epargne, entrer le taux.");
-				System.out.println("Option 2 :");
-				System.out.println("Le systeme affiche les donnees du compte choisi");
-				System.out.println("Option 3 :");
-				System.out.println("Ecrire une ligne comptable");
-				System.out.println("Option 4 :");
-				System.out.println("Quitter le programme");
-				System.out.println("Option 5 :");
-				System.out.println("Afficher de l'aide : Vous y etes ;) ");
+				help();
 				break;
 				
-				default :
-					System.out.println("Cette option n'est pas prévue par le système");
+			default :
+				System.out.println("Cette option n'est pas prévue par le système");
 			}		
-		} while (choice != 4);	
+		} while (choice != 4);
+	}
+	
+	// printing menu and return user' choice
+	public static byte mainMenu() {
+		byte choice;
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("1. Creer un compte");	
+		System.out.println("2. Afficher un compte");
+		System.out.println("3. Creer une ligne comptable");
+		System.out.println("4. Sortir");
+		System.out.println("5. De l'aide");
+		System.out.print("Votre choix : ");
+		// user choice 
+		choice = userInput.nextByte();
+		return choice;
+	}
+	
+	// getting out of the app
+	public static void out() {
+		System.out.print("Au revoir");
+		System.exit(0);
+	}
+	
+	// displaying some help
+	public static void help() {
+		System.out.println("Option 1 :");
+		System.out.println("Pour créer un compte courante entrer : C"); 
+		System.out.println("Pour créer un compte joint entrer : J");
+		System.out.println("Pour créer un compte epargne entrer : E");
+		System.out.println("- Entrer ensuite le numero du compte");
+		System.out.println("- Entrer alors la valeur creditee");
+		System.out.println("Dans le cas d'un compte epargne, entrer le taux.");
+		System.out.print("Option 2 :");
+		System.out.println(" Le systeme affiche les donnees du compte choisi");
+		System.out.print("Option 3 :");
+		System.out.println(" Ecrire une ligne comptable");
+		System.out.print("Option 4 :");
+		System.out.println(" Quitter le programme");
+		System.out.print("Option 5 :");
+		System.out.println(" Afficher de l'aide. Vous y etes ;) ");
+		System.out.println(" ");
 	}
 }
